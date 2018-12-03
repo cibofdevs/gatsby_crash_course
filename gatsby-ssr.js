21fe10f5de516ee16cbb1a8908458327fddd3ee4
Ahmad Wijaya <31590671+cibofdevs@users.noreply.github.com>
2018-12-03 18:19:18 +0700

    Gatsby Site 🚀

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,5 @@
 /**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file test.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
